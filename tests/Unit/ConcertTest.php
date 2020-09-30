6dc65e26b9b002b439b4ec120b03b558b501f63c
Catalin Vasile <contact@catalinvasile.dev>
2020-09-30 22:53:37 +0200

    Refactored unit test

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -4,10 +4,9 @@ declare(strict_types = 1);
 
 namespace Tests\Unit;
 
+use Carbon\Carbon;
 use Tests\TestCase;
 use App\Models\Concert;
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ConcertTest extends TestCase
@@ -17,16 +16,12 @@ class ConcertTest extends TestCase
     /** @test */
     public function fetched_date_is_formatted(): void
     {
-        // Create a concert with a known date
-        $concert = Factory::factoryForModel(Concert::class)->create(
-            [
+        $concert = Concert::factory()->create([
                 'date' => Carbon::parse('2016-12-01 8:00pm'),
-            ]);
+        ]);
 
-        // Retrieve the formatted date
         $date = $concert->formatted_date;
 
-        // Verify the date is formatted as expected
         $this->assertEquals('December 1, 2016', $date);
     }
 }
