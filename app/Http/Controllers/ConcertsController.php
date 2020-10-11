cdd358392302c44dfcbfe4d57040a459ad42d759
Catalin Vasile <contact@catalinvasile.dev>
2020-10-11 14:00:05 +0200

    Refactored controller to only retrieve published concernts

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\Concert;
@@ -8,7 +10,7 @@ class ConcertsController extends Controller
 {
     public function show($id)
     {
-        $concert = Concert::whereNotNull('published_at')->findOrFail($id);
+        $concert = Concert::published()->findOrFail($id);
         return view('concerts.show', ['concert' => $concert]);
     }
 }
