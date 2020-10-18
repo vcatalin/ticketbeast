f5216d1729307c7c73637ce8fe4eca581942083c
Catalin Vasile <contact@catalinvasile.dev>
2020-10-18 22:03:16 +0200

    Cleaned release test to adhere arrange act assert model

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -8,6 +8,7 @@ use App\Models\Ticket;
 use App\Reservation;
 use Illuminate\Support\Collection;
 use Mockery;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class ReservationTest extends TestCase
@@ -29,16 +30,19 @@ class ReservationTest extends TestCase
     /** @test */
     public function reserved_tickets_are_released_when_a_reservation_is_cancelled(): void
     {
-        $ticket1 = Mockery::mock(Ticket::class);
-        $ticket1->shouldReceive('release')->once();
-        $ticket2 = Mockery::mock(Ticket::class);
-        $ticket2->shouldReceive('release')->once();
-        $ticket3 = Mockery::mock(Ticket::class);
-        $ticket3->shouldReceive('release')->once();
+        $tickets = new Collection([
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+        ]);
 
-        $tickets = new Collection([$ticket1, $ticket2, $ticket3]);
         $reservation = new Reservation($tickets);
 
         $reservation->cancel();
+
+        foreach ($tickets as $ticket) {
+            /** @var MockInterface $ticket */
+            $ticket->shouldHaveReceived('release')->once();
+        }
     }
 }
