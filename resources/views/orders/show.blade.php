a41d879fb7b8734f7d46f2f8f72484d764e0850f
Catalin Vasile <contact@catalinvasile.dev>
2020-11-09 22:01:04 +0100

    Added dynamic fields to the order confirmation view

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -7,7 +7,7 @@
                 <div class="m-xs-b-6">
                     <div class="flex-baseline flex-spaced p-xs-y-4 border-b">
                         <h1 class="text-xl">Order Summary</h1>
-                        <a href="#" class="link-brand-soft">{{ $order->confirmation_number }}</a>
+                        <a href="{{ url("/orders/{$order->confirmation_number}") }}" class="link-brand-soft">{{ $order->confirmation_number }}</a>
                     </div>
                     <div class="p-xs-y-4 border-b">
                         <p>
@@ -22,8 +22,8 @@
                     <div class="card m-xs-b-5">
                         <div class="card-section p-xs-y-3 flex-baseline flex-spaced text-light bg-gray">
                             <div>
-                                <h1 class="text-xl wt-normal">No warning</h1>
-                                <p class="text-light-muted">with Cruel Hand and Backtrack</p>
+                                <h1 class="text-xl wt-normal">{{ $ticket->concert->title }}</h1>
+                                <p class="text-light-muted">{{ $ticket->concert->subtitle }}</p>
                             </div>
                             <div class="text-right">
                                 <strong>General Admission</strong>
@@ -39,12 +39,12 @@
                                         </div>
                                         <div class="media-body p-xs-l-4">
                                             <p class="wt-bold">
-                                                <time datetime="2011-10-16">
-                                                    Sunday, October 16, 2011
+                                                <time datetime="{{ $ticket->concert->date->format('Y-m-d H:i') }}">
+                                                    {{ $ticket->concert->date->format('l, F j, Y') }}
                                                 </time>
                                             </p>
                                             <p class="text-dark-soft">
-                                                Doors at 8:00PM
+                                                Doors open at {{ $ticket->concert->date->format('g:ia') }}
                                             </p>
                                         </div>
                                     </div>
@@ -55,11 +55,11 @@
                                             <x-zondicon-location class="zondicon text-brand-muted" />
                                         </div>
                                         <div class="media-body p-xs-l-4">
-                                            <p class="wt-bold">Music Hall of Williamsburg</p>
+                                            <p class="wt-bold">{{ $ticket->concert->venue }}</p>
                                             <div class="text-dark-soft">
-                                                <p>123 Main St. W</p>
+                                                <p>{{ $ticket->concert->venue_address }}</p>
                                                 <p>
-                                                    Brooklyn, New York 14259
+                                                    {{ $ticket->concert->city }}, {{ $ticket->concert->state }} {{ $ticket->concert->zip }}
                                                 </p>
                                             </div>
                                         </div>
@@ -69,7 +69,7 @@
                         </div>
                         <div class="card-section flex-baseline flex-spaced">
                             <p class="text-lg">{{ $ticket->code }}</p>
-                            <p>john.smith@example.com</p>
+                            <p>{{ $order->email }}</p>
                         </div>
                     </div>
                     @endforeach
--- tests/Browser/Pages/ViewOrderTest.php
+++ tests/Browser/Pages/ViewOrderTest.php
@@ -8,6 +8,7 @@ use App\Models\Concert;
 use App\Models\Order;
 use App\Models\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Carbon;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\DuskTestCase;
 
@@ -18,7 +19,17 @@ class ViewOrderTest extends DuskTestCase
     /** @test */
     public function user_can_view_their_order_confirmation(): void
     {
-        $concert = Concert::factory()->create();
+        $concert = Concert::factory()->published()->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2020 8:00pm'),
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]);
+
         $order = Order::factory()->create([
             'confirmation_number' => 'FOOBAR123',
             'amount' => '8500',
@@ -41,8 +52,18 @@ class ViewOrderTest extends DuskTestCase
         $response->assertViewHas('order', $order);
         $response->assertSee($order->confirmation_number);
         $response->assertSee('$85.00');
-        $response->assertSee('**** **** **** ' . $order->card_last_four);
+        $response->assertSee('**** **** **** 4242');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+
+        $response->assertSee('2020-12-13');
+        $response->assertSee('8:00pm');
     }
 }
