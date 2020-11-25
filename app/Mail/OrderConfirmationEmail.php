7b2a60fad98c7bf2961accfdf3c0c26e85f93f2b
Catalin Vasile <contact@catalinvasile.dev>
2020-11-25 21:37:11 +0100

    Extended test for fake order confirmation email to be sent out

---
+++ app/Mail/OrderConfirmationEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderConfirmationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Order $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
